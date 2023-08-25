186ec70f8a97976fb7924ae32d93e056a0a1e6ff
imba97 <mail@imba97.cn>
2023-08-26 00:48:55 +0800

    chore: 打包配置优化

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import { upperFirst } from 'lodash'
+import { get, keys, merge, isFunction, upperFirst } from 'lodash'
 
 import path from 'path'
 import glob from 'glob'
@@ -6,28 +6,70 @@ import glob from 'glob'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 
+const OutputKey = Object.freeze({
+  /**
+   * 页面 layout
+   */
+  Layout: 'layout',
+
+  /**
+   * hexo 打包使用的脚本
+   */
+  Scripts: 'scripts'
+})
+
 // src 打包对应的实际输出文件夹
 const outputDir = {
-  layout: 'source/js',
-  scripts: 'scripts'
+  [OutputKey.Layout]: 'source/js/layout',
+  [OutputKey.Scripts]: 'scripts'
 }
 
-export default glob.sync('build/**/*.js').map((file) => {
-  const [, outputKey, ...pathName] = file.split('/')
-  const name = pathName.map(upperFirst).join('').replace(/\.js$/, '')
-
-  return {
-    external: ['jquery', 'lodash', 'smooth-scrollbar'],
-    input: file,
-    output: {
-      name,
-      file: path.resolve(__dirname, outputDir[outputKey], ...pathName),
-      format: 'iife',
-      globals: {
-        jquery: '$',
-        lodash: '_'
+const configHandler = {
+  [OutputKey.Layout]({ name }) {
+    return {
+      external: ['jquery', 'lodash', 'smooth-scrollbar'],
+      output: {
+        name,
+        format: 'iife',
+        globals: {
+          jquery: '$',
+          lodash: '_'
+        }
       }
-    },
-    plugins: [resolve(), commonjs()]
+    }
   }
-})
+}
+
+export default keys(outputDir).reduce((configs) => {
+  const config = glob.sync(`build/**/*.js`).map((file) => {
+    const [, outputKey, ...pathName] = file.split('/')
+    const name = pathName.map(upperFirst).join('').replace(/\.js$/, '')
+
+    const outputFile = path.resolve(
+      __dirname,
+      outputDir[outputKey],
+      ...pathName
+    )
+
+    // 公共配置
+    const commonConfig = {
+      input: file,
+      output: {
+        file: outputFile
+      },
+      plugins: [resolve(), commonjs()]
+    }
+
+    // 获取打包配置处理函数
+    const handler = get(configHandler, outputKey)
+
+    // 根据打包代码不同的配置
+    const configOfKey = isFunction(handler) ? handler({ name }) : {}
+
+    return merge(commonConfig, configOfKey)
+  })
+
+  configs.push(...config)
+
+  return configs
+}, [])
