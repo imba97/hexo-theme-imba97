336d2122619f442eac7768c99c768658a94d5f4d
imba97 <mail@imba97.cn>
2023-08-26 19:36:48 +0800

    perf: index 代码迁移

--- source/js/layout/index.js
+++ source/js/layout/index.js
@@ -6,6 +6,32 @@ var Index = (function (exports, $, Scrollbar) {
             damping: 0.2,
             alwaysShowTracks: true
         });
+        NProgress.start();
+        window.onload = function () {
+            NProgress.done();
+        };
+        Index.generateFancybox();
+        $(document).on('pjax:send', function () {
+            NProgress.start();
+        });
+        $(document).on('pjax:complete', function () {
+            NProgress.done();
+            Index.generateFancybox();
+        });
+        $.fancybox.defaults.hash = false;
+        document.addEventListener('DOMContentLoaded', function () {
+            new Pjax({
+                elements: 'a[href]:not([href^="#"]):not([href="javascript:void(0)"]):not([pjax-fancybox]):not([notallow="return false;"]):not([notallow="return!1"]):not([target="_blank"]):not([target="view_window"]):not([href$=".xml"])',
+                selectors: [
+                    'head title',
+                    'head meta[name=keywords]',
+                    'head meta[name=description]',
+                    'script[data-pjax]',
+                    '#main'
+                ],
+                cacheBust: false
+            });
+        });
     });
     const generateFancybox = () => {
         $('.article-entry').each(function (i) {
--- src/layout/index.ts
+++ src/layout/index.ts
@@ -6,6 +6,41 @@ $(() => {
     damping: 0.2,
     alwaysShowTracks: true
   })
+
+  NProgress.start()
+  window.onload = function () {
+    NProgress.done()
+  }
+
+  Index.generateFancybox()
+
+  $(document).on('pjax:send', function () {
+    NProgress.start()
+  })
+
+  $(document).on('pjax:complete', function () {
+    NProgress.done()
+    Index.generateFancybox()
+  })
+
+  // 修复 fancybox 关闭时触发 pjax 的问题
+  ;($ as any).fancybox.defaults.hash = false
+
+  let pjax
+  document.addEventListener('DOMContentLoaded', function () {
+    pjax = new Pjax({
+      elements:
+        'a[href]:not([href^="#"]):not([href="javascript:void(0)"]):not([pjax-fancybox]):not([notallow="return false;"]):not([notallow="return!1"]):not([target="_blank"]):not([target="view_window"]):not([href$=".xml"])',
+      selectors: [
+        'head title', // 标题
+        'head meta[name=keywords]', // 关键词
+        'head meta[name=description]', // 描述
+        'script[data-pjax]', // script 标签添加 data-pjax
+        '#main'
+      ],
+      cacheBust: false
+    })
+  })
 })
 
 export const generateFancybox = () => {
