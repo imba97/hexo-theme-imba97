470933a63bd0fc7ff1c7728916f764348c4bd4a4
imba97 <mail@imba97.cn>
2023-10-15 19:55:45 +0800

    perf: 自动加载支持 CSS

--- scripts/helpers/auto-import.js
+++ scripts/helpers/auto-import.js
@@ -7,43 +7,50 @@ var fsExtra = require('fs-extra');
 const tree = [];
 // lib 目录
 const lib = 'lib';
+// 权重，权重越大引入越靠前
+const weight = {
+    jquery: 10
+};
+// 支持配置启用的库
+const enabledKey = ['fancybox'];
+// 启用
+const getEnabledLibrary = (key) => !~enabledKey.indexOf(key) ||
+    _.get(hexo.theme.config, `functions.${key}`, false);
+const topInfo = _.keys(weight).map((name) => ({
+    name,
+    path: `${lib}/${name}/`
+}));
 hexo.extend.helper.register('autoImport', function (type) {
     switch (type) {
-        case 'js':
-            // 权重，权重越大引入越靠前
-            const weight = {
-                jquery: 10
-            };
-            // 支持配置启用的库
-            const enabledKey = ['fancybox'];
-            const topInfo = _.keys(weight).map((name) => ({
-                name,
-                path: `${lib}/${name}/`
-            }));
-            // 启用
-            const enabled = _.reduce(enabledKey, (result, key) => _.assign(result, {
-                [key]: _.get(hexo.theme.config, `functions.${key}`, false)
-            }), {});
+        case 'js': {
             const js = tree.filter((file) => path.extname(file) === '.js');
-            const output = _(js)
-                .map((file) => file.replace(/^(?:.*)source\\(.*)$/, '$1').replaceAll('\\', '/'))
-                // 过滤出启用的
-                .filter((file) => {
-                const key = _.get(new RegExp(`${lib}/(.*?)/`).exec(file), '1');
-                return !key || !_.has(enabled, key) || enabled[key];
-            })
-                // 根据权重排序
-                .orderBy((file) => {
-                const info = _.find(topInfo, (info) => !!~file.indexOf(info.path));
-                return _.get(weight, `${info?.name}`, 0);
-            }, 'desc')
-                // 转 script
-                .map((file) => `<script src="/${file}"></script>`)
-                .join('\n');
+            const output = autoImportHandler(js, (file) => `<script src="/${file}"></script>`);
             return output;
+        }
+        case 'css': {
+            const css = tree.filter((file) => path.extname(file) === '.css');
+            const output = autoImportHandler(css, (file) => `<link rel="stylesheet" href="/${file}">`);
+            return output;
+        }
     }
     return '';
 });
+function autoImportHandler(files, outputHandler) {
+    return (_(files)
+        .map((file) => file.replace(/^(?:.*)source\\(.*)$/, '$1').replaceAll('\\', '/'))
+        // 过滤出启用的
+        .filter((file) => {
+        const key = _.get(new RegExp(`${lib}/(.*?)/`).exec(file), '1');
+        return getEnabledLibrary(key);
+    })
+        // 根据权重排序
+        .orderBy((file) => {
+        const info = _.find(topInfo, (info) => !!~file.indexOf(info.path));
+        return _.get(weight, `${info?.name}`, 0);
+    }, 'desc')
+        .map(outputHandler)
+        .join('\n'));
+}
 (function getDirectoryTree(dirPath) {
     const dir = fsExtra.readdirSync(dirPath);
     _.forEach(dir, (name) => {
--- src/scripts/helpers/auto-import.ts
+++ src/scripts/helpers/auto-import.ts
@@ -7,57 +7,75 @@ const tree: string[] = []
 // lib 目录
 const lib = 'lib'
 
+// 权重，权重越大引入越靠前
+const weight: Record<string, number> = {
+  jquery: 10
+}
+
+// 支持配置启用的库
+const enabledKey = ['fancybox']
+
+// 启用
+const getEnabledLibrary = (key: string): boolean =>
+  !~enabledKey.indexOf(key) ||
+  _.get(hexo.theme.config, `functions.${key}`, false)
+
+const topInfo = _.keys(weight).map((name) => ({
+  name,
+  path: `${lib}/${name}/`
+}))
+
 hexo.extend.helper.register('autoImport', function (type) {
   switch (type) {
-    case 'js':
-      // 权重，权重越大引入越靠前
-      const weight: Record<string, number> = {
-        jquery: 10
-      }
-
-      // 支持配置启用的库
-      const enabledKey = ['fancybox']
-
-      const topInfo = _.keys(weight).map((name) => ({
-        name,
-        path: `${lib}/${name}/`
-      }))
-
-      // 启用
-      const enabled = _.reduce(
-        enabledKey,
-        (result, key) =>
-          _.assign(result, {
-            [key]: _.get(hexo.theme.config, `functions.${key}`, false)
-          }),
-        {}
-      )
-
+    case 'js': {
       const js = tree.filter((file) => path.extname(file) === '.js')
 
-      const output = _(js)
-        .map((file) =>
-          file.replace(/^(?:.*)source\\(.*)$/, '$1').replaceAll('\\', '/')
-        )
-        // 过滤出启用的
-        .filter((file) => {
-          const key = _.get(new RegExp(`${lib}/(.*?)/`).exec(file), '1')
-          return !key || !_.has(enabled, key) || enabled[key]
-        })
-        // 根据权重排序
-        .orderBy((file) => {
-          const info = _.find(topInfo, (info) => !!~file.indexOf(info.path))
-          return _.get(weight, `${info?.name}`, 0)
-        }, 'desc')
-        // 转 script
-        .map((file) => `<script src="/${file}"></script>`)
-        .join('\n')
+      const output = autoImportHandler(
+        js,
+        (file) => `<script src="/${file}"></script>`
+      )
 
       return output
+    }
+
+    case 'css': {
+      const css = tree.filter((file) => path.extname(file) === '.css')
+
+      const output = autoImportHandler(
+        css,
+        (file) => `<link rel="stylesheet" href="/${file}">`
+      )
+
+      return output
+    }
   }
 
   return ''
 })
+
+function autoImportHandler(
+  files: string[],
+  outputHandler: (file: string) => string
+) {
+  return (
+    _(files)
+      .map((file) =>
+        file.replace(/^(?:.*)source\\(.*)$/, '$1').replaceAll('\\', '/')
+      )
+      // 过滤出启用的
+      .filter((file) => {
+        const key: string = _.get(new RegExp(`${lib}/(.*?)/`).exec(file), '1')!
+        return getEnabledLibrary(key)
+      })
+      // 根据权重排序
+      .orderBy((file) => {
+        const info = _.find(topInfo, (info) => !!~file.indexOf(info.path))
+        return _.get(weight, `${info?.name}`, 0)
+      }, 'desc')
+      .map(outputHandler)
+      .join('\n')
+  )
+}
 
 // 获取 source/lib 目录树
 ;(function getDirectoryTree(dirPath: string) {
