64d900e41202784bec33e98a4862c99ba84353f1
imba97 <mail@imba97.cn>
2023-10-15 19:09:58 +0800

    fix: pjax 不生效的问题

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -26,20 +26,17 @@ $(() => {
   // 修复 fancybox 关闭时触发 pjax 的问题
   ;($ as any).fancybox.defaults.hash = false
 
-  let pjax
-  document.addEventListener('DOMContentLoaded', function () {
-    pjax = new Pjax({
-      elements:
-        'a[href]:not([href^="#"]):not([href="javascript:void(0)"]):not([pjax-fancybox]):not([notallow="return false;"]):not([notallow="return!1"]):not([target="_blank"]):not([target="view_window"]):not([href$=".xml"])',
-      selectors: [
-        'head title', // 标题
-        'head meta[name=keywords]', // 关键词
-        'head meta[name=description]', // 描述
-        'script[data-pjax]', // script 标签添加 data-pjax
-        '#main'
-      ],
-      cacheBust: false
-    })
+  new Pjax({
+    elements:
+      'a[href]:not([href^="#"]):not([href="javascript:void(0)"]):not([pjax-fancybox]):not([notallow="return false;"]):not([notallow="return!1"]):not([target="_blank"]):not([target="view_window"]):not([href$=".xml"])',
+    selectors: [
+      'head title', // 标题
+      'head meta[name=keywords]', // 关键词
+      'head meta[name=description]', // 描述
+      'script[data-pjax]', // script 标签添加 data-pjax
+      '#main'
+    ],
+    cacheBust: false
   })
 })
 
