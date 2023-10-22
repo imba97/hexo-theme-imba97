d114b46c74bd7ffefd85fbb173778eb41e5df2cf
imba97 <mail@imba97.cn>
2023-10-22 18:13:11 +0800

    perf: 移动端菜单优化

--- src/layout/header.ts
+++ src/layout/header.ts
@@ -1,18 +1,46 @@
 import $ from 'jquery'
 
 $(() => {
-  $('body').on('click', '#menu-expand-button', () => {
-    const container = $('#menu-expand-container')
-    const button = $('#menu-expand-button')
+  const Menu = {
+    container: $('#menu-expand-container'),
+    button: $('#menu-expand-button'),
+    isShow: false,
+    show() {
+      this.isShow = true
+      this.container.show()
+      this.button.find('[i-fad-h-expand]').hide()
+      this.button.find('[i-iconamoon-close-bold]').show()
+    },
+    hide() {
+      this.isShow = false
+      this.container.hide()
+      this.button.find('[i-fad-h-expand]').show()
+      this.button.find('[i-iconamoon-close-bold]').hide()
+    }
+  }
 
-    if (container.is(':hidden')) {
-      container.show()
-      button.find('[i-fad-h-expand]').hide()
-      button.find('[i-iconamoon-close-bold]').show()
+  $('body').on('click', '#menu-expand-button', () => {
+    console.log('Menu.isShow', Menu.isShow)
+    const a = Menu.container.find('a')
+
+    const hide = () => {
+      Menu.hide()
+    }
+
+    if (Menu.isShow) {
+      Menu.hide()
+
+      a.each((_index, aElement) => {
+        aElement.removeEventListener('click', hide)
+      })
     } else {
-      container.hide()
-      button.find('[i-fad-h-expand]').show()
-      button.find('[i-iconamoon-close-bold]').hide()
+      Menu.show()
+
+      a.each((_index, aElement) => {
+        aElement.addEventListener('click', hide, {
+          once: true
+        })
+      })
     }
   })
 })
