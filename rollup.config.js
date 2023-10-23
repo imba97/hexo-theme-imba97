c5d5f8ba518444b0ccf27c5642f354c7c98fadcc
imba97 <mail@imba97.cn>
2023-10-23 22:54:15 +0800

    feat: rollup alias

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import { get, keys, merge, isFunction, upperFirst } from 'lodash'
 import path from 'path'
 import glob from 'glob'
 
+import { defineConfig } from 'rollup'
+import alias from '@rollup/plugin-alias'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import terser from '@rollup/plugin-terser'
@@ -16,6 +18,11 @@ const OutputKey = Object.freeze({
   Page: 'page',
 
   /**
+   * 页面 worker
+   */
+  Worker: 'worker',
+
+  /**
    * hexo 打包使用的脚本
    */
   Scripts: 'scripts'
@@ -24,22 +31,30 @@ const OutputKey = Object.freeze({
 // src 打包对应的实际输出文件夹
 const outputDir = {
   [OutputKey.Page]: 'source/js/page',
+  [OutputKey.Worker]: 'source/js/worker',
   [OutputKey.Scripts]: 'scripts'
 }
 
-const configHandler = {
-  [OutputKey.Page]({ name }) {
-    return {
-      external: ['jquery', 'lodash', 'smooth-scrollbar'],
-      output: {
-        name,
-        format: 'iife',
-        globals: {
-          jquery: '$',
-          lodash: '_'
-        }
+const iifeConfig = ({ name }) =>
+  defineConfig({
+    external: ['jquery', 'lodash', 'smooth-scrollbar'],
+    output: {
+      name,
+      format: 'iife',
+      globals: {
+        jquery: '$',
+        lodash: '_'
       }
     }
+  })
+
+const configHandler = {
+  [OutputKey.Page](options) {
+    return iifeConfig(options)
+  },
+
+  [OutputKey.Worker](options) {
+    return iifeConfig(options)
   },
 
   [OutputKey.Scripts]() {
@@ -54,7 +69,6 @@ const configHandler = {
 
 export default keys(outputDir).reduce((configs) => {
   const config = glob.sync(`build/**/*.js`).map((file) => {
-    console.log(file)
     const [, outputKey, ...pathName] = file.split(/\\|\//)
     const name = pathName.map(upperFirst).join('').replace(/\.js$/, '')
 
@@ -65,13 +79,25 @@ export default keys(outputDir).reduce((configs) => {
     )
 
     // 公共配置
-    const commonConfig = {
+    const commonConfig = defineConfig({
       input: file,
       output: {
         file: outputFile
       },
-      plugins: [resolve(), commonjs(), ...(!isDev ? [terser()] : [])]
-    }
+      plugins: [
+        alias({
+          entries: [
+            {
+              find: /^@/,
+              replacement: path.resolve(path.resolve(__dirname), 'build')
+            }
+          ]
+        }),
+        resolve(),
+        commonjs(),
+        ...(!isDev ? [terser()] : [])
+      ]
+    })
 
     // 获取打包配置处理函数
     const handler = get(configHandler, outputKey)
