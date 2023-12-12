178746c67f07fa7149f692da05b873daae55cd07
imba97 <mail@imba97.cn>
2023-12-12 23:20:04 +0800

    perf: worker 尝试

--- src/worker/search.worker.ts
+++ src/worker/search.worker.ts
@@ -1,5 +1,17 @@
 const receivedMessage = (event: MessageEvent) => {
-  self.postMessage('test content')
+  const xhr = new XMLHttpRequest()
+
+  xhr.addEventListener('readystatechange', () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText)
+        self.postMessage(data)
+      }
+    }
+  })
+
+  xhr.open('GET', '/search.json')
+  xhr.send()
 }
 
 self.addEventListener('message', receivedMessage)
